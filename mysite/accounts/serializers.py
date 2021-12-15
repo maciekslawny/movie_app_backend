6cd97c214a8c3eb6573a4881d137dbaafb9cae07
Maciej <maciekslawny@gmail.com>
2021-12-15 09:51:24 +0100

    updated user create function, pbkdf2 to password

--- mysite/accounts/serializers.py
+++ mysite/accounts/serializers.py
@@ -2,23 +2,14 @@ from accounts.models import CustomUser
 from rest_framework import serializers
 
 
-class RegisterUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ("email", "user_name", "password")
-        extra_kwargs = {"password": {"write_only": True}}
-
-    def create(self, validated_data):
-        password = validated_data.pop("password", None)
-        # as long as the fields are the same, we can just use this
-        instance = self.Meta.model(**validated_data)
-
-        instance.set_password(password)
-        instance.save()
-        return instance
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = "__all__"
+
+    def create(self, validated_data):
+        user = super(UserSerializer, self).create(validated_data)
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
